2af74cea7ac5e8d429d4dd2e3b9f3ae4a5122a51
ijabine <49118549+ijabine@users.noreply.github.com>
2019-10-27 18:03:55 +0100

    Create stats.R

---
+++ R_code/stats.R
@@ -0,0 +1,21 @@
+##                                V1        V2        V3        V4        V5
+## 1                            Beta     pbeta     qbeta     dbeta     rbeta
+## 2                        Binomial    pbinom    qbinom    dbinom    rbinom
+## 3                          Cauchy   pcauchy   qcauchy   dcauchy   rcauchy
+## 4                      Chi-Square    pchisq    qchisq    dchisq    rchisq
+## 6                     Exponential      pexp      qexp      dexp      rexp
+## 7                               F        pf        qf        df        rf
+## 8                           Gamma    pgamma    qgamma    dgamma    rgamma
+## 9                       Geometric     pgeom     qgeom     dgeom     rgeom
+## 10                 Hypergeometric    phyper    qhyper    dhyper    rhyper
+## 11                       Logistic    plogis    qlogis    dlogis    rlogis
+## 12                     Log Normal    plnorm    qlnorm    dlnorm    rlnorm
+## 13              Negative Binomial   pnbinom   qnbinom   dnbinom   rnbinom
+## 14                         Normal     pnorm     qnorm     dnorm     rnorm
+## 15                        Poisson     ppois     qpois     dpois     rpois
+## 16                      Student t        pt        qt        dt        rt
+## 17              Studentized Range    ptukey    qtukey    dtukey    rtukey
+## 18                        Uniform     punif     qunif     dunif     runif
+## 19                        Weibull  pweibull  qweibull  dweibull  rweibull
+## 20    Wilcoxon Rank Sum Statistic   pwilcox   qwilcox   dwilcox   rwilcox
+## 21 Wilcoxon Signed Rank Statistic psignrank qsignrank dsignrank rsignrank
